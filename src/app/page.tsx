35e5ed3ca5579fb56c1b192b1cade0086ea86281
manishimwe1 <emmymanishimwem@gmail.com>
2024-10-25 15:01:30 -0700

    fix build error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-import Header from "../components/Header";
-import Hero from "../components/Hero";
 import Services from "@/components/Services";
 import ShowCase from "@/components/ShowCase";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import Hero from "../components/Hero";
 
 export default function Home() {
   return (
